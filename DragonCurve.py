5fe797fadcbc625b1f1f07203bd7fbbf2d0af0eb
Jacopo Tissino <jacopo@tissino.it>
2015-02-16 13:09:07 +0100

    aggiunto codice di prova da http://lost-theory.org/python/dynamicimg.html

--- DragonCurve.py
+++ DragonCurve.py
@@ -22,6 +22,8 @@
 #  
 #  
 import math
+import Image, ImageDraw
+from random import randint as rint
 
 lista = []
 
@@ -41,3 +43,20 @@ def creastringa(n):
 
 n = input("Quanti numeri vuoi calcolare?\n")
 creastringa(n)
+
+def randgradient():
+    img = Image.new("RGB", (300,300), "#FFFFFF")
+    draw = ImageDraw.Draw(img)
+
+    r,g,b = rint(0,255), rint(0,255), rint(0,255)
+    dr = (rint(0,255) - r)/300.
+    dg = (rint(0,255) - g)/300.
+    db = (rint(0,255) - b)/300.
+    for i in range(300):
+        r,g,b = r+dr, g+dg, b+db
+        draw.line((i,0,i,300), fill=(int(r),int(g),int(b)))
+
+    img.save("out.png", "PNG")
+
+if __name__ == "__main__":
+    randgradient()
