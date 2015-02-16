0f19f30cedcde01e9bf8b3895b3b1d128ed25564
Jacopo Tissino <jacopo@tissino.it>
2015-02-16 16:01:40 +0100

    Changed function names + fixed some lines not displaying + changes output type

--- DragonCurve.py
+++ DragonCurve.py
@@ -24,27 +24,27 @@
 import math
 import Image, ImageDraw
 from random import randint as rint
-img = Image.new( 'RGB', (255,255), "black")
+img = Image.new( 'RGB', (1000,1000), "white") # create a new white image
 draw = ImageDraw.Draw(img)
 
-def calcola(n):
+def compute(n):
     if n == 0:
         return 0
     else:
         l = pow(2, math.floor(math.log(n,2))+1) - n - 1	
-        return (calcola(l) + 1)%4
+        return (compute(l) + 1)%4
         #Recursive function to calculate a single number in the series.
 
-def creastringa(n):
-    stringa = ''
+def createstring(n):
+    string = ''
     for i in range(0, n):
-        stringa += str(calcola(i))
+        string += str(compute(i))
         #Create the string 01212321...
-    return stringa
+    return string
 
 def line(x, y, length, width, direction):
     if direction == 0:
-        for i in range(x, x-length, 1):
+        for i in range(x-length, x, 1):
             for j in range(y, y+width, 1):
                 pixels[i,j] = (100, 100, 100)
     elif direction == 1:
@@ -57,24 +57,23 @@ def line(x, y, length, width, direction):
                 pixels[i,j] = (100, 100, 100)
     elif direction == 3:
         for i in range(x, x+width, 1):
-            for j in range(y, y-length, 1):
+            for j in range(y-length, y, 1):
                 pixels[i,j] = (100, 100, 100)
     else:
         print("Invalid direction!")
 
 
-n = int(input("Quanti numeri vuoi calcolare?\n"))
-stringa = creastringa(n)
+n = int(input("How many lines do you want?\n"))
+string = createstring(n)
 
-img = Image.new( 'RGB', (255,255), "white") # create a new white image
 pixels = img.load() # create the pixel map
-x = img.size[0] / 2
+x = img.size[0] / 2 # start at the center
 y = img.size[1] / 2
 
 while n > 0:
     length = 5
     width = 1
-    k = int(stringa[n-1])
+    k = int(string[n-1])
     line(x, y, length, width, k)
     if k == 0:
         x -= length
@@ -86,4 +85,4 @@ while n > 0:
         y -= length
     n = n-1
 
-img.save("out.png", "PNG")
+img.show("out.png", "PNG")
